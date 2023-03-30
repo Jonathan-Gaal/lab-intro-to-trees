820cc36a7a2cfc890c9a1f97017ff8f3008a0553
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-03-30 18:42:10 -0400

    passed 1 test

--- index.js
+++ index.js
@@ -10,7 +10,26 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    // returns referenct to root if exists
+    if (!this.root) {
+      return null;
+    } else {
+      return this.root;
+    }
+    // returns reference to leaf if exists
+    //   const results = node.children.map((child) => {
+    //     if (node.children.length === 0) {
+    //       return node;
+    //     }
+    //     return results;
+    //   });
+    // }
+  }
 }
+
+const treeA = new Tree();
+
+console.log(treeA);
 
 module.exports = { TreeNode, Tree };
